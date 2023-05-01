9d35dce9839ed2642c30e9d137ac72ade5a95f56
JeremiasRy <ryttari.jeremias@gmail.com>
2023-05-01 09:10:17 +0300

    feat: add transaction to test to isolate tests

--- BackendTests/BaseRepoTests.cs
+++ BackendTests/BaseRepoTests.cs
@@ -25,13 +25,17 @@ public class BaseRepoTests : IClassFixture<DbTestFixture>
     public async void CreateBaseRepos()
     {
         using var context = Fixture.CreateContext();
+        context.Database.BeginTransaction();
         
         // city
         
         CityRepo cityRepo = new(context);
         var cityResult = await cityRepo.CreateOneAsync(new City() { Name = "Espoo" });
+        context.ChangeTracker.Clear();
         Assert.True(cityResult?.Name == "Espoo");
+           
         cityResult = await cityRepo.CreateOneAsync(new City() { Name = "Tampere" });
+        context.ChangeTracker.Clear();
         Assert.True(cityResult is null);
         var total = await cityRepo.GetAllAsync(null);
         Assert.True(total.Count() == 3);
@@ -40,8 +44,10 @@ public class BaseRepoTests : IClassFixture<DbTestFixture>
 
         InstrumentRepo instrumentRepo = new(context);
         var instrumentResult = await instrumentRepo.CreateOneAsync(new Instrument() { Name = "Trombone" });
+        context.ChangeTracker.Clear();
         Assert.True(instrumentResult?.Name == "Trombone");
         instrumentResult = await instrumentRepo.CreateOneAsync(new Instrument() { Name = "Guitar" });
+        context.ChangeTracker.Clear();
         Assert.True(instrumentResult is null);
         var instrumentTotal = await instrumentRepo.GetAllAsync(null);
         Assert.True(instrumentTotal.Count() == 9);
@@ -50,8 +56,10 @@ public class BaseRepoTests : IClassFixture<DbTestFixture>
 
         GenreRepo genreRepo = new(context);
         var genreResult = await genreRepo.CreateOneAsync(new Genre() { Name = "Jazz" });
+        context.ChangeTracker.Clear();
         Assert.True(genreResult?.Name == "Jazz");
         genreResult = await genreRepo.CreateOneAsync(new Genre() { Name = "Metal" });
+        context.ChangeTracker.Clear();
         Assert.True(genreResult is null);
         var genreTotal = await genreRepo.GetAllAsync(null);
         Assert.True(genreTotal.Count() == 6);
@@ -60,11 +68,13 @@ public class BaseRepoTests : IClassFixture<DbTestFixture>
     public async void GetAllBaseRepos()
     {
         using var context = Fixture.CreateContext();
+        context.Database.BeginTransaction();
+
         CityRepo cityRepo = new(context);
         var result = await cityRepo.GetAllAsync(new BaseQueryOptions() { Limit = 1 });
         Assert.True(result.Count() == 1);
         result = await cityRepo.GetAllAsync(new NameFilter() { Name = "Espoo" });
-        Assert.True(result.First().Name == "Espoo");
+        Assert.True(!result.Any());
         result = await cityRepo.GetAllAsync(new NameFilter() { Name = "Tampere" });
         Assert.True(result.First().Name == "Tampere");
         result = await cityRepo.GetAllAsync(new NameFilter() { Name = "hsfdpoer" });
